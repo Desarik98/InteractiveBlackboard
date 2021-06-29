b6d187928d544ceec63762efb5910b91d03caadc
Daniel Mejia <daniel.mejia@mail.escuelaing.edu.co>
2021-06-29 12:55:50 -0500

    Finalizado Laboratorio InteractiveBlackBoard falta README y pruebas de usuario

---
+++ src/main/java/co/edu/escuelaing/interactiveblackboard/InteractiveBlackboardApplication.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.interactiveblackboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveBlackboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InteractiveBlackboardApplication.class, args);
+    }
+
+}
